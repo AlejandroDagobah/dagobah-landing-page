8aaef3a88785b01d15a3aea8d841c00740317b08
dagobah <alejandrodagobah@gmail.com>
2023-01-25 23:29:59 -0500

    dynamic websites

--- src/components/Websites.jsx
+++ src/components/Websites.jsx
@@ -1,36 +1,49 @@
 import React from "react";
 import Atropos from "atropos/react";
 import styles from "../style"
+import websites from "../websites";
+
+
 export default function Website() {
+
+    let cards
+
+    let websitesCards = websites.map((websiteCard, cardIndex) =>{
+
+        let buttons = websiteCard.buttons.map((item, index) =>{
+            return <span key={index} className="bg-secondary text-primary p-2 px-3 mr-4 rounded-md text-xs">{item}</span>
+
+        })
+
+        let website = <Atropos key={cardIndex} className="my-atropos" activeOffset={40} shadowScale={0}>
+
+            <div className={`border-secondary border-2 h-[32rem] sm:h-[20rem] w-full mt-4 py-12 px-5 sm:px-8 z-10 flex flex-col sm:flex-row sm:justify-between sm:max-w-1/2 sm:mt-3`}> 
+                <div className="website md:w-1/2 flex flex-col sm:justify-center lg:pr-16" data-atropos-offset="5">
+                    <h3>{websiteCard.title}</h3>
+                    <p className="text-[12px] mt-3">{websiteCard.description}</p>
+                    <div className="mt-6">
+                        {buttons}
+                    </div>
+                </div>
+
+                <div className="mt-10 sm:mt-0 xs:w-full xs:px-20 ss:px-32 ss:mt-0 sm:px-8 sm:ml-12 sm:-mt-10 md:w-1/2 md:ml-0 md:px-0">
+                    <img src={websiteCard.websiteImage} className={`${styles.websitesImg} -z-20 max-w-none -left-[30%] w-[140%] sm:absolute sm:left-[45%] sm:w-[48%] sm:top-[12%] lg:w-[47%] lg:top-[11%]`} data-atropos-offset="-7"/>
+
+                    <img src={websiteCard.phoneImage} className={`${styles.websitesImg} -z-10 -top-[40%] left-[57%] w-[70%] sm:absolute sm:left-[76%] sm:top-[36%] sm:w-[20%] lg:w-[22%] lg:top-[24%]`} data-atropos-offset="-5"/>
+                </div>
+            </div>
+
+        </Atropos>
+
+        return website
+    })
 
     return(
         <div className=" my-28">
         <h2 className="flex items-center whitespace-nowrap w-full after:content-[''] after:ml-3 after:block after:relative after:top-1  after:w-full after:h-px after:bg-secondary"><span className="mr-1 text-aleRed">02.</span> Trabajo Destacado</h2>
             
-        <Atropos 
-            className="my-atropos"
-            activeOffset={40}
-            shadowScale={0}    
-        >
-            <div className="border-secondary border-2 h-[32rem] sm:h-[20rem] w-full py-12 px-8 sm:px-8 z-10 flex flex-col sm:flex-row sm:justify-between sm:flex-row sm:max-w-1/2 sm:mt-3"> 
-                <div className="website md:w-1/2 flex flex-col sm:justify-center" data-atropos-offset="5">
-                    <h3>Joyería Flor de la Vida</h3>
-                    <p className="text-[12px] mt-3">Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto.</p>
-                    <div className="mt-6">
-                        <span className="bg-secondary text-primary p-2 px-3 rounded-md text-xs">e-commerce</span>
-                        <span className="bg-secondary text-primary p-2 px-3 ml-2 rounded-md text-xs">Web design</span>
-                    </div>
-                </div>
-
-                <div className="mt-10 sm:mt-0 xs:w-full xs:px-20 ss:px-0 ss:pl-16 sm:pl-18">
-                    <img src="img/web-fdlv-img.png" className={`${styles.websitesImg} -z-20 max-w-none -left-[30%] w-[140%] sm:top-[12%]`} data-atropos-offset="-7"/>
-
-                    <img src="img/phone-fdlv-img.png" className={`${styles.websitesImg} -z-10 -top-[40%] left-[57%] w-[70%] sm:-top-[40%] sm:w-[60%]`} data-atropos-offset="-5"/>
-                </div>
-            </div>
-
-        </Atropos>
+        {websitesCards}
        
-    </div>
+        </div>
     )
 }
---
+++ src/websites.js
@@ -0,0 +1,30 @@
+const websites = [
+    {
+        title:'Joyería Flor de la Vida',
+        description:'Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto.',
+        buttons: ['e-commerce', 'Web design'],
+        websiteImage: 'img/web-fdlv-img.png',
+        phoneImage:'img/phone-fdlv-img.png',
+        isRight:true
+    },
+    {
+        title:'QualityB2B',
+        description:'Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto.',
+        buttons: ['e-commerce', 'Web design'],
+        websiteImage: 'img/website-portfolio-quality-2.png',
+        phoneImage:'img/phone-portfolio-quality-2.png',
+        isRight:false
+
+    },
+    {
+        title:'Fenix.ec',
+        description:'Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto.',
+        buttons: ['e-commerce', 'Web design'],
+        websiteImage: 'img/website-portfolio-fenix.png',
+        phoneImage:'img/phone-portfolio-fenix.png',
+        isRight:false
+
+    }
+]
+
+export default websites
