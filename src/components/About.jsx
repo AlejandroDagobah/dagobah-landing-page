3cb1d6744d379fa1b5aa1c7d7dc2751c4ee4a8a0
Alejandro Dagobah <alejandrodagobah@gmail.com>
2025-02-18 01:55:49 -0500

    ADD About text changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,40 +8,27 @@ export default function About() {
             <div className="flex flex-col sm:flex-row sm:justify-between">
                 <div className="sm:basis-1/2">
                     <h2 className="flex whitespace-nowrap items-center w-full after:content-[''] after:ml-3 after:block after:relative after:top-1  after:w-full after:h-px after:bg-secondary"><span className="mr-1 text-aleRed">01.</span> <span></span> Acerca de mí</h2>
-                    <p className="mb-6 mt-2 leading-5">Soy un joven de 23 años apasionado por la tecnología, que ama aprender nuevas cosas cada día. Tengo mucho entusiasmo a la hora de trabajar en nuevos proyectos.
-    	                                                            <br/><br/>
-                                                                    Desde muy pequeño me interesaban las computadoras, los videojuegos, el internet y el arte, estas pasiones evolucionaron al aprender <span className="text-aleBlue">informática, desarrollo de videojuegos, desarrollo web y diseño gráfico</span> respectivamente. 
-                                                                    <br /><br />
-                                                                    Ahora me dedico a trabajar desarrollando proyectos y soluciones digitales y he tenido el placer de trabajar para diferentes empresas elaborando su presencia digital. 
-                                                                    <br /><br />
-                                                                    Estas son algunas de mis <span className="text-aleBlue">habilidades </span>destacadas:
+                    <p className="mb-6 mt-2 leading-5 aboutText">
+                            
+                        Soy un desarrollador obsesionado con la creación de sitios web, aplicaciones y experiencias digitales <b>visualmente impresionantes</b>, que a su vez mantengan un <b>alto rendimiento</b>, fluidez y facilidad de uso. Mi enfoque está en fusionar la <b>creatividad artística</b> con una <b>ingeniería sólida</b>, diseñando interfaces que no solo brillan por su estética, sino que también ofrezcan una <b>experiencia intuitiva</b> y sin fricciones. Mi misión es entregar productos que no solo se vean bien, sino que hagan que los usuarios disfruten cada segundo de interacción.
+                        <br/>
+                        <br/>
+                        Actualmente soy <b>Lead Developer en <a className="text-aleRed" href="https://www.hipopink.com" target="_blank">Hipopink</a></b>, lidero el equipo de desarrollo web, supervisando todo el proceso desde el descubrimiento del cliente y la planificación del proyecto hasta el diseño, desarrollo y despliegue de sitios web personalizados. Soy un defensor de <b>soluciones escalables y de alto rendimiento</b>, utilizando React, Gatsby y GraphQL para arquitecturas Headless CMS.
+                        <br/>
+
+                        <br/>
+                        He tenido la oportunidad de trabajar como desarrollador interno en <b>empresas privadas</b>, así como en <b>agencias de publicidad</b>, pasando también por el mundo <b>freelance</b>. Esto me ha brindado una base sólida en <b>diseño</b>, <b>marketing</b>, <b>SEO</b> y <b>diseño de producto</b>. Combino todas estas habilidades para entregar productos digitales que no solo sean visualmente atractivos, sino que también cumplan una <b>función estratégica</b> para el cliente.
+                        <br/>
+
+                        <br/>
+                        En mi tiempo libre, me puedes encontrar leyendo, dibujando, manteniéndome al tanto de lo que sucede en el mundo, tomando café, pasando tiempo con mi familia o quizás aprendiendo algún <b>arte shinobi</b>. 🥷
+
                     </p>
-                    <div className="flex text-sm font-sourceCodePro">
-                        <ul className="list-disc ml-4 marker:text-aleRed">
-                            <li className="py-2">HTML + CSS</li>
-                            <li className="py-2">Javascript (ES6+)</li>
-                            <li className="py-2">React</li>
-                            <li className="py-2">Wordpress</li>
-                            <li className="py-2">Unity - C#</li>
-                            <li className="py-2">Node.js (Básico)</li>
-                            <li className="py-2">PHP (Básico)</li>
-                        </ul>
-                        <ul className="list-disc ml-20 marker:text-aleRed">
-                            <li className="py-2">Photoshop</li>
-                            <li className="py-2">Illustrator</li>
-                            <li className="py-2">After Effects</li>
-                            <li className="py-2">Premiere Pro</li>
 
-                            <li className="py-2">Figma</li>
-                            <li className="py-2">SEO</li>
-
-                            <li className="py-2 ">Inglés Conversacional</li>
-                        </ul>
-                    </div>
                 </div>
 
                 <div className="flex justify-center sm:justify-end grow my-6 sm:my-0 sm:p-0 sm:w-[200%] md:w-[100%] sm:basis-1/2">
-                    <img src="./img/alejandro-small-2.png" className="self-start my-8 ml-8 w-[80%] grayscale hover:grayscale-0 transition duration-200" alt="" />
+                    <img src="./img/retrato-2.png" className="self-start my-8 ml-8 w-[80%] grayscale hover:grayscale-0 transition duration-200" alt="" />
                 </div>
             
             </div>
